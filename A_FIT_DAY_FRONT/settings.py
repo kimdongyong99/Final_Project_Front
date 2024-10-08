640b7b3fb039393ecb8a1991d4c5e40b93650528
lyj7111 <lyj7111@gmail.com>
2024-10-08 10:11:45 +0900

    first commit

--- A_FIT_DAY_FRONT/settings.py
+++ A_FIT_DAY_FRONT/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from .my_settings import MY_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5bod1r97r2k5lv9#&o_suqz)f6ikp%m$&w8imyerv-+00yn_il"
+SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
