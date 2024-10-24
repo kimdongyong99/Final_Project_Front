d2dabb5f59ac1635da3e4eb4358dcce9915b9d56
kyuho <noaetoile1@gmail.com>
2024-10-24 10:42:59 +0900

    Update wsgi.py
    주석 삭제

--- A_FIT_DAY_FRONT/wsgi.py
+++ A_FIT_DAY_FRONT/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for A_FIT_DAY_FRONT project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
